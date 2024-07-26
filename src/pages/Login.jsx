3a7eafd3b187837fb6c669036ae919a33ecf2d16
AscensaoJ <123513572+AscensaoJ@users.noreply.github.com>
2024-07-26 12:27:47 -0700

    add persistence

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { usePasswordToggle } from '/util/passwordUtils';
 import CustomColorMode from '/util/toggleColorMode';
